7fc0b7d66a29828a097ee18b1395a96961a5a48b
SumaiyaHamed <sumaiyahit@gmail.com>
2023-07-05 11:08:27 +0400

    Create 4 packages controllers , models, Repositories, and Service

---
+++ src/main/java/com/PlayerRankingAPISystem/PlayerRankingAPISystem/PlayerRankingApiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.PlayerRankingAPISystem.PlayerRankingAPISystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerRankingApiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerRankingApiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PlayerRankingAPISystem/PlayerRankingAPISystem/PlayerRankingApiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlayerRankingAPISystem.PlayerRankingAPISystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerRankingApiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
