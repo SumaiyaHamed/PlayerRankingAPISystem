e61869c6bb845ec0fca7ba4e7dfd836ed509a55a
SumaiyaHamed <sumaiyahit@gmail.com>
2023-07-05 15:57:24 +0400

    write a query to git player by id and git all players

--- src/main/java/com/PlayerRankingAPISystem/PlayerRankingAPISystem/Repositories/PlayerRepository.java
+++ src/main/java/com/PlayerRankingAPISystem/PlayerRankingAPISystem/Repositories/PlayerRepository.java
@@ -1,9 +1,22 @@
 package com.PlayerRankingAPISystem.PlayerRankingAPISystem.Repositories;
 
+import com.PlayerRankingAPISystem.PlayerRankingAPISystem.Models.Player;
 import com.PlayerRankingAPISystem.PlayerRankingAPISystem.Models.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Score, Integer> {
+
+    @Query("select p from players  c where p.id = :playerId")
+    Player getPlayerById(@Param("playerId") Integer id);
+
+    @Query("select p from players p")
+    List<Player> getAllPlayers();
+
+
 }
