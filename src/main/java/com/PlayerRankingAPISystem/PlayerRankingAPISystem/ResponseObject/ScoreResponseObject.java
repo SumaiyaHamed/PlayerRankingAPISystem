a2c19183d1abc9237ef1ce4e4fa6e5024bc02d7f
SumaiyaHamed <sumaiyahit@gmail.com>
2023-07-10 09:22:41 +0400

    create convertRequestToResponse method in class ScoreResponseObject

--- src/main/java/com/PlayerRankingAPISystem/PlayerRankingAPISystem/ResponseObject/ScoreResponseObject.java
+++ src/main/java/com/PlayerRankingAPISystem/PlayerRankingAPISystem/ResponseObject/ScoreResponseObject.java
@@ -1,4 +1,39 @@
 package com.PlayerRankingAPISystem.PlayerRankingAPISystem.ResponseObject;
 
-public class ScoreResponseObject { 
+import com.PlayerRankingAPISystem.PlayerRankingAPISystem.Models.Player;
+import com.PlayerRankingAPISystem.PlayerRankingAPISystem.Models.Score;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Data
+@Builder
+public class ScoreResponseObject {
+
+    private Long id;
+
+    private int scoreValue;
+
+    public static ScoreResponseObject convertRequestToResponse(Score scoreRequestFromUser) {
+        return ScoreResponseObject.builder()
+                .id(scoreRequestFromUser.getId())
+                .scoreValue(scoreRequestFromUser.getScoreValue())
+                .build();
+    }
+
+    public static List<ScoreResponseObject> convertRequestListToResponseList(List<Score> scoreRequestFromUser) {
+        List<ScoreResponseObject> scoreResponseList = new ArrayList<>();
+        if (!scoreRequestFromUser.isEmpty()) {
+            for (Score scoreRequest : scoreRequestFromUser) {
+                scoreResponseList.add(convertRequestToResponse(scoreRequest));
+            }
+        }
+        return scoreResponseList;
+    }
 }
